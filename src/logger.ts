6f9c199b01bd323806078eb65c27ef04d2df3264
sjdhome <sjdhoome@gmail.com>
2023-12-18 18:53:30 +0800

    Use pino as logger

---
+++ src/logger.ts
@@ -0,0 +1,15 @@
+import pino from "pino";
+
+const logger =
+  process.env.NODE_ENV === "development"
+    ? pino({
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+          },
+        },
+      })
+    : pino();
+
+export default logger;
