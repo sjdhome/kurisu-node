69fed2875a6fe5ee3f16b34404e556b03a53d17e
sjdhome <sjdhoome@gmail.com>
2023-12-23 21:14:50 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import process = require("process");
-import Koa = require("koa");
+import process from "process";
+import Koa, { Context, Next } from "koa";
+import Router from "@koa/router";
+import logger from "./logger.js";
+import { getPost, getPosts } from "./controller/post.js";
+import { getPostsByTag, getTagsByPost } from "./controller/post_tag.js";
 
 const app = new Koa();
 const port = process.env.PORT ?? 3000;
-app.listen(port);
+
+const router = new Router();
+router.get("/blog/post", getPosts);
+router.get("/blog/post/:id", getPost);
+router.get("/blog/post/:id/tag", getTagsByPost);
+router.get("/blog/tag/:id/post", getPostsByTag);
+
+const request2log = async (ctx: Context, next: Next) => {
+  logger.info(ctx);
+  await next();
+};
+
+app
+  .use(request2log)
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(port);
